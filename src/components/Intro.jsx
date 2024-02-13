5e79e5cde124b541f40d2d9655a25df4ba22128e
nidaonder <nidaaonder@gmail.com>
2024-02-13 16:37:17 +0300

    added intro component.

---
+++ src/components/Intro.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+function Intro({startTest}) {
+
+    const info = [
+        {
+          id: 1,
+          description: "The test consists of 10 questions.",
+        },
+        {
+          id: 2,
+          description: "Once you press the button your time will start.",
+        },
+        {
+          id: 3,
+          description:
+            "Each question remains on the screen for a maximum of 30 seconds.",
+        },
+        {
+          id: 4,
+          description: "Answer options will not appear for the first 10 seconds.",
+        },
+        {
+          id: 5,
+          description:
+            "After clicking one of the options or completing 30 seconds, you will move on to the new question.",
+        },
+        {
+          id: 6,
+          description: "It will not be possible to return to past questions.",
+        },
+      ];
+
+  return (
+    <div className="introduction">
+        <ul className="info">
+          {info.map((item, index) => (
+            <li key={item.id}>{item.description}</li>
+          ))}
+        </ul>
+        <p>GOOD LUCK!</p>
+        <button id="start" onClick={startTest}>START</button>
+      </div>
+  )
+}
+
+export default Intro
